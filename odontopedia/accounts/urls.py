ab754fd55c570c0476c07b95917cc1c8754735df
andongoshev <andon.go6ev@gmail.com>
2025-02-04 17:14:56 +0200

    Stable version for authentications, reset password, change password, email smtp setted up and working. No styling added

--- odontopedia/accounts/urls.py
+++ odontopedia/accounts/urls.py
@@ -15,8 +15,10 @@ urlpatterns = [
         template_name='registration/password_reset_form.html',
         email_template_name='registration/password_reset_email.html'
     ), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
          name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
