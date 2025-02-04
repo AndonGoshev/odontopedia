820bbd6c97ba908d246ea743410a5fc8f43f5384
andongoshev <andon.go6ev@gmail.com>
2025-02-04 15:14:16 +0200

    added password reset feauture

--- odontopedia/accounts/urls.py
+++ odontopedia/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from odontopedia.accounts.views import RegistrationView, CustomLoginView, CustomLogoutView, AuthGoogle, \
     CustomPasswordChangeView, CustomPasswordChangeDoneView
@@ -9,5 +10,13 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('auth-receiver/', AuthGoogle.as_view(), name='auth-receiver'),
     path('password-change/', CustomPasswordChangeView.as_view(), name='password-change'),
-    path('password-change-done/', CustomPasswordChangeDoneView.as_view(), name='password-change-done' ),
+    path('password-change-done/', CustomPasswordChangeDoneView.as_view(), name='password-change-done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html'
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
